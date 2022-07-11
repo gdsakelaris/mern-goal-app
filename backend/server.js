ee716fde77f5c46a3cd39cfa96ebcf9ad9e51a5c
gdsakelaris <gsakelar@depaul.edu>
2022-07-11 13:45:06 -0500

    commit again lmao

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const colors = require("colors");
 const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
-connectDB();
-
 const app = express();
-
+app.listen(port, () => console.log(`Server started on port ${port}`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/api/goals", require("./routes/goalRoutes"));
 
+const { errorHandler } = require("./middleware/errorMiddleware");
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const connectDB = require("./config/db");
+connectDB();
+
+/////////////////////////////////
+const colors = require("colors");
+/////////////////////////////////
